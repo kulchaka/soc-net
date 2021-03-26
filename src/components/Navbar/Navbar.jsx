ab19c86aa1b845e6e3fc01637d8553965d9ff42e
Andrii <kulchaka@gmail.com>
2021-03-26 10:35:47 +0100

    NavLink, don't reaload page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import nav from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className={nav.nav}>
       <ul className={nav.links}>
-        <li className={nav.item__link}><a href="/profile">Profile</a></li>
-        <li className={nav.item__link}><a href="/dialogs">Message</a></li>
-        <li className={nav.item__link}><a href="/news">News</a></li>
-        <li className={nav.item__link}><a href="/music">Music</a></li>
-        <li className={nav.item__link}><a href="/settings">Settings</a></li>
+        <li className={nav.item__link}><NavLink to="/profile">Profile</NavLink></li>
+        <li className={nav.item__link}><NavLink to="/dialogs">Message</NavLink></li>
+        <li className={nav.item__link}><NavLink to="/news">News</NavLink></li>
+        <li className={nav.item__link}><NavLink to="/music">Music</NavLink></li>
+        <li className={nav.item__link}><NavLink to="/settings">Settings</NavLink></li>
       </ul>
     </nav>
   )
