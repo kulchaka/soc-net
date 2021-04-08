488560499646d3baaeafeb874324cb572f15d9c8
Andrii <kulchaka@gmail.com>
2021-04-08 11:09:52 +0200

    refaktoring state -> ActionCreator & ActionType, lesson 39

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,82 +1,6 @@
-// let rerenderTree = () => {
-//   console.log('state chg')
-// }
-
-// const state = {
-//   dialogsPage: {
-//     dialogsData: [
-//       {
-//         id: '01',
-//         name: 'John'
-//       },
-//       {
-//         id: '02',
-//         name: "Andrii"
-//       },
-//       {
-//         id: '03',
-//         name: "Peter"
-//       }
-//     ],
-//     messagesData: [
-//       {
-//         id: '01',
-//         message: 'Hello'
-//       },
-//       {
-//         id: '02',
-//         message: "How are you?!"
-//       },
-//       {
-//         id: '03',
-//         message: "Yo!"
-//       }
-//     ]
-//   },
-//   profilePage: {
-//     postsData: [
-//       {
-//         id: '01',
-//         text: 'Hello, how are you?',
-//         likesCount: '2'
-//       },
-//       {
-//         id: '02',
-//         text: "Hi, this is my first POST ))",
-//         likesCount: '5'
-//       },
-//       {
-//         id: '03',
-//         text: "Bye!"
-//       }
-//     ],
-//     textArea: 'enter text'
-//   }
-//
-// }
-
-// export const addPost = () => {
-//   const newPost = {
-//     id: '04',
-//     text: state.profilePage.textArea,
-//     likesCount: '0'
-//   }
-//   state.profilePage.postsData.push(newPost)
-//   state.profilePage.textArea = ''
-//   rerenderTree(state)
-// }
-
-// export const postChange = (text) => {
-//   state.profilePage.textArea = text;
-//   rerenderTree(state)
-// }
-
-// export const subscribe = (observer) => {
-//   rerenderTree = observer
-// }
-
-
 //store - OOP
+const ADD_POST = 'ADD-POST';
+const POST_UPDATE = 'POST-UPDATE';
 
 let store = {
   _state: {
@@ -157,18 +81,14 @@ let store = {
   }
 }
 
-export const addPostActionCreator = () => {
-  return {
-    type: 'ADD-POST'
-  }
-}
+export const addPostActionCreator = () => ({ type: ADD_POST });
 
-export const updateNewPostTextActionCreator = (text) => {
-  return {
-    type: 'POST-UPDATE',
+
+export const updateNewPostTextActionCreator = (text) => ({
+    type: POST_UPDATE,
     newText: text
   }
-}
+);
 
 export default store;
 window.store = store
