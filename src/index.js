49ccdd64df616b7bc33369bf168ffac9cddc3040
Andrii <kulchaka@gmail.com>
2021-04-19 11:46:37 +0200

    add REDUX npm install redux --save

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css';
 import ReactDOM from "react-dom";
 import App from "./App";
-import store from "./redux/state";
+import store from "./redux/store";
 
 const rerenderTree = (state) => {
   ReactDOM.render(
