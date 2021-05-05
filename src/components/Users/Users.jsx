a352f6609a61aba1dda272dbcb46e7fc53fcbc25
Andrii <kulchaka@gmail.com>
2021-05-05 10:11:34 +0200

    class component User - lesson 52

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,47 +3,52 @@ import s from './Users.module.css'
 import * as axios from "axios";
 import userLogo from '../../assets/img/logo.png'
 
-const Users = (props) => {
-  // console.log(props)
 
-  const getState = () => {
-    if (props.users.length === 0) {
+class Users extends React.Component {
+
+  getUsers = () => {
+    if (this.props.users.length === 0) {
 
       axios.get('https://social-network.samuraijs.com/api/1.0/users')
         .then(response => {
           console.log(response.data.items)
-          props.setUsers(response.data.items)
+          this.props.setUsers(response.data.items)
         })
     }
   }
-  return <>
-    <button onClick={getState}>Get State</button>
-    {props.users.map(u =>
-      <div key={u.id} className={s.block}>
-        <div className={s.block__logo}>
-          <img src={u.photos.small != null ? u.photos.small : userLogo} alt="logo" className={s.logo}/>
-          {
-            u.followed
-              ? <button className={s.button} onClick={() => {
-                props.unfollow(u.id)
-              }}>UNFOLLOW</button>
-              : <button className={s.button} onClick={() => {
-                props.follow(u.id)
-              }}>FOLLOW</button>
-          }
-        </div>
-        <div className={s.block__info}>
-          <div className={s.block__name}>
-            <p className={s.name}>{u.name}</p>
-            <p className={s.status}>{u.status}</p>
-          </div>
-          <div className={s.block__location}>
-            <p className={s.country}>{'u.location.country'}</p>
-            <p className={s.city}>{'u.location.city'}</p>
-          </div>
-        </div>
-      </div>)}
-  </>
+
+  render() {
+    return <>
+      <button onClick={this.getUsers}>Get State</button>
+      {
+        this.props.users.map(u =>
+          <div key={u.id} className={s.block}>
+            <div className={s.block__logo}>
+              <img src={u.photos.small != null ? u.photos.small : userLogo} alt="logo" className={s.logo}/>
+              {
+                u.followed
+                  ? <button className={s.button} onClick={() => {
+                    this.props.unfollow(u.id)
+                  }}>UNFOLLOW</button>
+                  : <button className={s.button} onClick={() => {
+                    this.props.follow(u.id)
+                  }}>FOLLOW</button>
+              }
+            </div>
+            <div className={s.block__info}>
+              <div className={s.block__name}>
+                <p className={s.name}>{u.name}</p>
+                <p className={s.status}>{u.status}</p>
+              </div>
+              <div className={s.block__location}>
+                <p className={s.country}>{'u.location.country'}</p>
+                <p className={s.city}>{'u.location.city'}</p>
+              </div>
+            </div>
+          </div>)
+      }
+    </>
+  }
 }
 
 export default Users
