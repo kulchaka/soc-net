964124b8bc2133e923b0550c6f5d93389f0aa171
Andrii <kulchaka@gmail.com>
2021-06-07 11:04:13 +0200

    add LOGIN into HTML lesson-61

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,14 @@ import {NavLink} from "react-router-dom";
 const Header = () => {
   return (
     <header className={s.header}>
-      <NavLink to='/'>
-      <div className={s.header__img}></div>
-      </NavLink>
+      <div className={s.header__block}>
+        <NavLink to='/'>
+          <div className={s.header__img}></div>
+        </NavLink>
+        <div className={s.header__login}>
+          <NavLink to='/login'>Login</NavLink>
+        </div>
+      </div>
     </header>
   )
 }
