2c31e1899d8ae4d7812b42ce2a58827a2bd80692
Andrii <kulchaka@gmail.com>
2021-06-09 10:29:57 +0200

    use axios.create, lesson-62

--- src/API/API.js
+++ src/API/API.js
@@ -1,7 +1,15 @@
 import * as axios from "axios";
 
+const instance = axios.create({
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  withCredentials: true,
+  headers: {
+    'API-KEY': '1959b0a8-82db-4788-8377-a222c6f69ee2'
+  }
+})
+
 export const getUserAPI = (currentPage, pageSize) => {
-  return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {
+  return instance.get(`users?page=${currentPage}&count=${pageSize}`, {
     withCredentials: true
   })
     .then(response => response.data)
